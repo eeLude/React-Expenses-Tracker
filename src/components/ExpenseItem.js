22da093716717210c4b78443b92937268b228b32
eeLude <eemil.juhani@gmail.com>
2024-09-08 15:06:47 +0300

    css tyylit, propsit, uusi komponentti

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,13 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
-    <div>
-      <div className="expense-item">
-        <div>Jan 1th 2023</div>
-        <div className="expense-item__description">
-          <h2>Car Insurance</h2>
-          <div className="expense-item__price">$294.67</div>
-        </div>
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
