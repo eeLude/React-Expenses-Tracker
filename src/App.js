4c1736f321514c31d3b818e89a58f2a3f4482a5e
eeLude <eemil.juhani@gmail.com>
2024-09-23 14:27:40 +0300

    +wrapper component +arrowfunctions +react import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import Expenses from "./components/Expenses";
+import React from "react";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -8,7 +9,7 @@ function App() {
       amount: 94.12,
       date: new Date("2022-07-14"),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date("2023-01-12") },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date("2022-08-12") },
     {
       id: "e3",
       title: "Car Insurance",
@@ -19,7 +20,7 @@ function App() {
       id: "e4",
       title: "New Desk (Wooden)",
       amount: 450,
-      date: new Date("2022-05-12"),
+      date: new Date("2023-01-09"),
     },
   ];
 
@@ -29,6 +30,6 @@ function App() {
       <p>Eemil Luodes / VAMK / BIT</p>
     </div>
   );
-}
+};
 
 export default App;
