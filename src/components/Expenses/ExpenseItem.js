1dd29c37290ea7d198791d3f8f27c1af2d13e89b
eelude <eemil.juhani@gmail.com>
2024-09-30 14:36:48 +0300

    card fix

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
-import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-
   const [title, setTitle] = useState(props.title);
 
   const clickHandler = () => {
@@ -17,7 +16,7 @@ const ExpenseItem = (props) => {
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
     </Card>
