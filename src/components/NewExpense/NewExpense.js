451055470d4fefeea89565419881e4c1ed2e2c92
eeLude <eemil.juhani@gmail.com>
2024-10-23 21:48:45 +0300

    show and hide new expense menu

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false); //input field tilamuuttuja
+
+  const startEditingHandler = () => {
+    //
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
   const SaveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -10,13 +21,27 @@ const NewExpense = (props) => {
     };
     props.onAddExpense(expenseData);
   };
-  return (
-    //
-    <div className="new-expense">
-      {/* Passing SaveExpenseDataHandler as a prop to ExpenseForm */}
-      <ExpenseForm onSaveExpenseData={SaveExpenseDataHandler} />
-    </div>
-  );
+
+  if (isEditing === false) {
+    return (
+      <div className="new-expense">
+        <button type="button" onClick={startEditingHandler}>
+          Add New Expense
+        </button>
+      </div>
+    );
+  }
+
+  if (isEditing === true) {
+    return (
+      <div className="new-expense">
+        <ExpenseForm
+          onSaveExpenseData={SaveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      </div>
+    );
+  }
 };
 
 export default NewExpense;
